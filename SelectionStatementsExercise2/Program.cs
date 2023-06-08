1b1f90e5f6d8963d4dbf29115d57ab082ece6c97
felix adnel <flixadnel@gmail.com>
2023-06-08 01:19:22 -0500

    second commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -11,10 +11,26 @@
                 switch (subject.ToLower())
                 {
                     case "spanish":
-                        Console.WriteLine("spanish is awesome");
+                        Console.WriteLine("Spanish is great i am trying to learn my self. ");
                         break;
+                    case "math":
+                        Console.WriteLine("Math is a fascinating subject that requires logic and problem-solving skills.");
+                        break;
+                    case "science":
+                        Console.WriteLine("Science helps us understand the natural world and fuels curiosity and discovery.");
+                        break;
+                    case "history":
+                        Console.WriteLine("History allows us to learn from the past and gain insights into civilization.");
+                        break;
+                    case "english":
+                        Console.WriteLine("English is awesome it enhances our communication skills and nurtures creativity through literature.");
+                        break;
+                    case "art":
+                        Console.WriteLine("Art is a wonderful medium of self-expression and allows us to explore our imagination.");
+                        break;
+                   
                     default:
-                        Console.WriteLine($"ahh i havent taken that before. { subject } sounds great!");
+                        Console.WriteLine($"Ahh i havent taken that before. { subject } sounds great!");
                         break;
                
                 }
