270884b48a1420f64564379e641f107bf6e9b6c3
felix adnel <flixadnel@gmail.com>
2023-06-08 01:18:40 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your fvorite school subject?");
+
+            var subject = Console.ReadLine();
+            {
+                switch (subject.ToLower())
+                {
+                    case "spanish":
+                        Console.WriteLine("spanish is awesome");
+                        break;
+                    default:
+                        Console.WriteLine($"ahh i havent taken that before. { subject } sounds great!");
+                        break;
+               
+                }
+
+
+            }
+
         }
     }
 }
